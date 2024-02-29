b2c14eee8ecbe42ee395edf54309dd81073b9eaa
north-tower <muneshmyke@gmail.com>
2024-02-29 16:08:12 +0300

    mg

--- src/components/AddHotel.js
+++ src/components/AddHotel.js
@@ -22,13 +22,13 @@ function AddHotel() {
       const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-          await axios.post('http://localhost:5000//api/v1/addHotel', newHotel);
+          await axios.post('http://localhost:5000/api/v1/addHotel', newHotel);
           // Display success message
-          window.alert('Driver added successfully');
+          window.alert('Hotel added successfully');
           // Redirect to Drivers component
           navigate('/hotels');
         } catch (error) {
-          console.error('Error adding new driver:', error);
+          console.error('Error adding new hotel:', error);
         }
       };
   return (
@@ -51,7 +51,7 @@ function AddHotel() {
           <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
         </div>
         <div class="relative z-0 w-full mb-5 group">
-          <input name="Address" value={newHotel.Rating} onChange={handleInputChange}  class="block py-2.5 px-0 w-full
+          <input name="Rating" value={newHotel.Rating} onChange={handleInputChange}  class="block py-2.5 px-0 w-full
           text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500
           focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
           <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Rating</label>
