c764fe6e21043c119ce7167931d67f16ea34c7d6
north-tower <muneshmyke@gmail.com>
2024-03-01 14:16:36 +0300

    qw

--- src/components/AddTrip.js
+++ src/components/AddTrip.js
@@ -42,7 +42,7 @@ function AddTrip() {
       text-sm text-gray-500 dark:text-gray-400 duration-300 transform 
       -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0
        rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto 
-       peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Driver Id</label>
+       peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Driver Name</label>
     </div>
     <div class="relative z-0 w-full mb-5 group">
       <input name="hotelId" value={newTrip.hotelId} onChange={handleInputChange} 
@@ -52,7 +52,7 @@ function AddTrip() {
       text-sm text-gray-500 dark:text-gray-400 duration-300 transform 
       -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0
        rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto 
-       peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Hotel Id</label>
+       peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Hotel Name</label>
     </div>
     <div class="relative z-0 w-full mb-5 group">
       <input name="startTime" value={newTrip.startTime} onChange={handleInputChange}  class="block py-2.5 px-0 w-full
--- src/components/Trips.js
+++ src/components/Trips.js
@@ -43,12 +43,12 @@ return (
         font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" to="/addtrip">
                 Add Trip
             </NavLink>
-    <div className="App">
+    {/* <div className="App">
       <div style={{ margin: '0 auto', marginTop: '1%' ,marginBottom: '1%' ,marginLeft:'40%' , marginRight:'40%'}}>
         <input className="form-control me-2" onChange={handleSearchChange} value={search} type="text" 
         placeholder="Search Trips"></input>
       </div>
-    </div>
+    </div> */}
               
     <div>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
