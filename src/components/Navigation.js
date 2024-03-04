87c11223ce33dc86d08dee7a27cb55a260b1ec84
Miki <muneshmyke@gmail.com>
2024-03-04 11:50:20 +0300

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -104,9 +104,12 @@ function Navigation() {
     <div class="space-y-1 px-2 pb-3 pt-2">
       
       <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+      <NavLink to="/"  class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3
+              py-2 text-base font-medium">Hotels</NavLink>
+      <NavLink to"/trips" href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 
+              text-base font-medium">Trips</NavLink>
+      <NavLink to"/drivers" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 
+              text-base font-medium">Drivers</NavLink>
     </div>
   </div>
 </nav>
