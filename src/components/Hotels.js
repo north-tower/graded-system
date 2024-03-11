a89f059bf547e0edc21579f5f5f283b44563dc21
Miki <muneshmyke@gmail.com>
2024-03-11 09:43:45 +0300

    Update Hotels.js

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -48,6 +48,9 @@ return (
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
+  <th scope="col" class="px-6 py-3">
+                  UUID
+                </th>
                 <th scope="col" class="px-6 py-3">
                   Hotel Name
                 </th>
@@ -65,6 +68,9 @@ return (
           <tbody>
             {filtered.map(hotel => (
              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+             {hotel.id}
+             </th>
              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
              {hotel.Name}
              </th>
