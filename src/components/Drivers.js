bcf368fee1b0c58f6eb5bc378b968c80007e510c
Miki <muneshmyke@gmail.com>
2024-03-11 09:41:58 +0300

    Update Drivers.js

--- src/components/Drivers.js
+++ src/components/Drivers.js
@@ -55,6 +55,9 @@ const Drivers = () => {
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
+     <th scope="col" class="px-6 py-3">
+                    UUID
+                </th>
                 <th scope="col" class="px-6 py-3">
                     First name
                 </th>
@@ -68,6 +71,9 @@ const Drivers = () => {
         <tbody>
         {filtered.map(driver => (
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+           <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {driver.id}
+                </th>
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 {driver.firstName}
                 </th>
